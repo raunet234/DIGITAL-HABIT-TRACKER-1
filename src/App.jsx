f51019170c823675e84cb06407ac3376338d3599
raunet234 <rauneet234@gmail.com>
2025-04-26 21:07:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-center mt-8">Digital Habit Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
